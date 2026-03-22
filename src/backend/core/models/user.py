d963e64a25f67aa823fa4e25f5c467b604c5d72d
ygrec4ka <sam06ser@mail.ru>
2026-03-22 14:29:49 +0400

    create user model

---
+++ src/backend/core/models/user.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from backend.core.models import Base
+from backend.core.models.mixins import IdIntPk
+from backend.core.types import UserIdType
+
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class User(Base, IdIntPk, SQLAlchemyBaseUserTable[UserIdType]):
+    pass
+
+    @classmethod
+    async def get_db(cls, session: "AsyncSession"):
+        yield SQLAlchemyUserDatabase(session, cls)
+
+    def __str__(self):
+        return self.email
