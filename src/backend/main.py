226a9a87993e7b0381d5f5963edabc838ab97844
ygrec4ka <sam06ser@mail.ru>
2026-03-22 18:00:30 +0400

    include api router to main app

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
+from backend.api import router as api_routes
 
 app = FastAPI()
+
+app.include_router(api_routes)
 
 
 @app.get("/")
@@ -10,8 +13,5 @@ def health_check():
     return {"status": "ok"}
 
 
-
 if __name__ == "__main__":
-    uvicorn.run(
-        app, host="0.0.0.0", port=8000
-    )
+    uvicorn.run(app, host="127.0.0.1", port=8000)
