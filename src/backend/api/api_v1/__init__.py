2a108afad3d00ca0c43f8209882a827cfc8cb69b
Samsa016 <drops-001@mail.ru>
2026-03-23 15:10:19 +0400

    update front, refactor backend

--- src/backend/api/api_v1/__init__.py
+++ src/backend/api/api_v1/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from backend.core import settings
 from .users import router as users_router
+from .auth import router as auth_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -9,3 +10,4 @@ router = APIRouter(
 
 
 router.include_router(users_router)
+router.include_router(auth_router)
---
+++ src/frontend/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "CoreSupport | Digital Agency",
+  description: "Next Generation Platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${outfit.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
