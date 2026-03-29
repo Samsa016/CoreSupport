5bb05ff413f5dc4888ecea9b706e49a70d056436
ygrec4ka <sam06ser@mail.ru>
2026-03-29 14:04:53 +0400

    feat: update user schemas

--- src/backend/core/schemas/user.py
+++ src/backend/core/schemas/user.py
@@ -1,10 +1,13 @@
+from typing import Optional
 from fastapi_users import schemas
 
+from backend.core.models.user import UserRole
 from backend.core.types import UserIdType
 
 
 class UserRead(schemas.BaseUser[UserIdType]):
-    pass
+    role: UserRole
+    is_working: bool
 
 
 class UserCreate(schemas.BaseUserCreate):
@@ -12,4 +15,4 @@ class UserCreate(schemas.BaseUserCreate):
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    is_working: Optional[bool] = None
